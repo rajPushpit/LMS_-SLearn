88ccb3ec5ab0730a56a6afc1dfb024f245bf3092
Pushpit Raj <pushpitraj19jan@gmail.com>
2026-02-11 23:33:48 +0530

    correct dep

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './views/base/Ind.css'
+import './views/base/ind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
