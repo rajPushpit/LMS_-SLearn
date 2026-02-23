be93283988ef27e72b42d6917f9c6b74528d1941
Pushpit Raj <pushpitraj19jan@gmail.com>
2026-02-23 20:03:33 +0530

    fix mysql railway connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,16 +99,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         }
 #     }
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('MYSQL_DATABASE'),
-        'USER': env('MYSQL_USER'),
-        'PASSWORD': env('MYSQL_PASSWORD'),
-        'HOST': env('MYSQL_HOST'),
-        'PORT': env('MYSQL_PORT'),
-    }
+    'default': dj_database_url.parse(env('MYSQL_PUBLIC_URL'))
 }
+
 
 
 
